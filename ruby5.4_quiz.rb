0388faf74db8b47ada33173ef137acd868e89c61
jiimms <jiimms@yahoo.com>
2016-03-11 16:42:55 +0530

    first

---
+++ ruby5.4_quiz.rb
@@ -0,0 +1,28 @@
+Quiz
+
+	Take the Quiz
+		1) Blocks can be denoted with 
+			a) do…each 
+			b) { } (correct)
+			c) [ ] 
+
+		2) ‘a’..’f’ will print 
+			a) a b c d e f (correct)
+			b) A B C D E F 
+			c) a b c d e 
+
+		3)1…5 will print 
+			a) 1 2 3 4 (correct)
+			b) 1 2 3 4 5 
+			c) 5 4 3 2 1 
+			c) 22  
+
+		4) Procedures are denoted with ………… class 
+			a) Proc (correct)
+			b) Lambda 
+			c) Procedure 
+
+		5)Proc and Lambda differs in 
+			a) Argument (correct)
+			b) Exceptions 
+			c) call method  
